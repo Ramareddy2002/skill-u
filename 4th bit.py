0b6fd7ef86215f5d99dd6bee9df0412ce518f683
Alekhya-02 <84336949+Alekhya-02@users.noreply.github.com>
2021-06-21 08:59:30 +0530

    Add files via upload

---
+++ 4th bit.py
@@ -0,0 +1,31 @@
+n=int(input())
+rem=0
+s=''
+while True:
+    rem=n%2
+    print(rem)
+    s=s+str(rem)
+    n=n//2
+    if n<=1:
+        print(1)
+        s+=str(1)
+        break
+
+print('4th bit',s[3])
+
+'''
+12
+0
+0
+1
+1
+4th bit 1
+
+23
+1
+1
+1
+0
+1
+4th bit 0
+'''
