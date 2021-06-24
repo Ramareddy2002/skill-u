94a7bcd5d17bcb4a5d0c168e947a020dc3f18319
Alekhya-02 <84336949+Alekhya-02@users.noreply.github.com>
2021-06-24 08:54:50 +0530

    Add files via upload

---
+++ closed path.py
@@ -0,0 +1,17 @@
+def cp(n):
+    if n==0 or n==4 or n==6 or n==9:
+        return 1
+    elif n==8:
+        return 2
+    else:
+        return 0
+def cpsum(t):
+    s=0
+    while t:
+        rem=t%10
+        t=t//10
+        s+=cp(rem)
+    return s
+t=int(input())
+print(cpsum(t))
+    
