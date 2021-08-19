1c8b59218ea328a54962477c963cab2f16008f52
Alekhya-02 <84336949+Alekhya-02@users.noreply.github.com>
2021-08-19 20:43:06 +0530

    Add files via upload

---
+++ len of string without len().py
@@ -0,0 +1,7 @@
+a=input('Enter string: ')
+n=0
+for i in a:
+    n+=1
+
+print(n)
+    
---
+++ string loops.py
@@ -0,0 +1,20 @@
+a=input('enter name: ')
+for i in range(0,len(a)):
+    print(i,a[i])
+
+for i in a:
+    print(i)
+    
+'''enter name: Harry Potter
+0 H
+1 a
+2 r
+3 r
+4 y
+5  
+6 P
+7 o
+8 t
+9 t
+10 e
+11 r'''
